e1a75cc4990ac686b8ff58ca00dae85ff0858c32
Gal R <gal8156@gmail.com>
2024-10-12 18:52:16 +0300

    status codes

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 import openai
 import os
 
 app = Flask(__name__)
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+
+@app.errorhandler(404)
+def not_found(error): 
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(401)
+def not_found(error): 
+    return render_template("401.html"), 401
+
+
+@app.route('/500')
+def error_500():
+    return render_template('500.html'), 500
+
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     response = openai.ChatCompletion.create(
@@ -22,8 +38,16 @@ def home():
 @app.route('/get')
 def get_bot_response():
     userText = request.args.get('msg')
-    response = get_completion(userText)
+    
+    if userText.lower() == "unauthorized":
+        abort(401)
+    
+    try:
+        response = get_completion(userText)
+    except openai.error.AuthenticationError:
+        abort(500) 
     return response
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
